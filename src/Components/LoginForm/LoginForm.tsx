10df663b13b5441e360ec683cde92725baba495a
Kim Ewing <pekar.kimberly@gmail.com>
2024-08-24 08:33:13 -0600

    adds functionality to display a message to the user if an incorrect email or password is entered.

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -8,6 +8,7 @@ interface User {
 const LoginForm = (): React.JSX.Element => {
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
+    const [isError, setIsError] = useState<boolean>(false)
 
     // when the user clicks the login button, execute a post request
     // if the response.ok, route the user to their dashboard
@@ -43,44 +44,58 @@ const LoginForm = (): React.JSX.Element => {
             email,
             password,
         }
-        console.log("user:", user)
-        authenticateUser(user)
+        if (email && password) {
+            authenticateUser(user)
+            clearInputs()
+        }
+        else {
+            setIsError(true)
+        }
     }
 
     const authenticateUser = (user: User) => {
         return fetch('https://turlink-be-53ba7254a7c1.herokuapp.com/api/v1/sessions', {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/json',  
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(user)
         })
-        .then(response => {
-            if(!response.ok) {
-                throw new Error ("Incorrect username or password.")
-            }
-            return response.json()
-        })
-        .then(response => console.log("response:", response))
-        .catch(error => console.log(error))
+            .then(response => {
+                if (!response.ok) {
+                    setIsError(true)
+                    throw new Error("Incorrect username or password.")             
+                }
+                return response.json()
+            })
+            .then(response => console.log("response:", response))
+            .catch(error => console.log(error))
+    }
+
+    const clearInputs = () => {
+        setEmail('')
+        setPassword('')
     }
 
     return (
-        <form>
-            <input
-                type="text"
-                placeholder="email"
-                value={email}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="password"
-                value={password}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
-            />
-            <button type="button" onClick={handleLogin}>Login</button>
-        </form>
+        <div>
+            <form>
+                <input
+                    type="text"
+                    placeholder="email"
+                    value={email}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+                />
+                <button type="button" onClick={handleLogin}>Login</button>
+            </form>
+            {isError && <p>Incorrect Email or Password</p>}
+        </div>
     )
 }
 
