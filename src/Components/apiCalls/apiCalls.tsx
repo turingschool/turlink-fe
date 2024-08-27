0153bb3a8ca5aef47e64657e4db1efb83337cdb4
David Swatzell <thatsstrikethree@gmail.com>
2024-08-27 11:11:18 -0500

    add api call for getting tags for a link

--- src/Components/apiCalls/apiCalls.tsx
+++ src/Components/apiCalls/apiCalls.tsx
@@ -6,3 +6,12 @@ export const getTags = async () => {
     const data = await response.json();
     return data.data;
   };
+
+  export const getTagsForLink = async (linkId: string) => {
+    const response = await fetch(`https://turlink-be-53ba7254a7c1.herokuapp.com/api/v1/tags?link=${linkId}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch tags for link');
+    }
+    const data = await response.json();
+    return data.data.attributes.tags;
+  };
