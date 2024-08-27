5850cc71339bcb61bf24fa71d367ce7a203f367c
David Swatzell <thatsstrikethree@gmail.com>
2024-08-27 11:18:55 -0500

    add useeffect for tags

--- src/Components/Tags/Tags.tsx
+++ src/Components/Tags/Tags.tsx
@@ -16,4 +16,28 @@ type TagsProps = {
     const [allTags, setAllTags] = useState<Tag[]>([]);
     const [linkTags, setLinkTags] = useState<Tag[]>([]);
     const [showTags, setShowTags] = useState(false);
-  
+    
+    useEffect(() => {
+    const fetchTags = async () => {
+        const tags = await getTags();
+        setAllTags(tags);
+      };
+      fetchTags();
+
+      const fetchLinkTags = async () => {
+        const tags = await getTagsForLink(linkId);
+        setLinkTags(tags);
+      };
+      fetchLinkTags();
+    }, [linkId]);
+
+    const handleAddTag = async (tagId: string) => {
+        const updatedTags = await addTagToLink(linkId, tagId);
+        setLinkTags(updatedTags);
+      };
+    
+      const handleRemoveTag = async (tagId: string) => {
+        const updatedTags = await removeTagFromLink(linkId, tagId);
+        setLinkTags(updatedTags);
+      };
+    
