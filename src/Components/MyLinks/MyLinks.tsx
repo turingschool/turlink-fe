725387c3909745acee62193ea62ecc46610ec63f
David Swatzell <thatsstrikethree@gmail.com>
2024-08-29 15:33:10 -0500

    add useeffect for my links

--- src/Components/MyLinks/MyLinks.tsx
+++ src/Components/MyLinks/MyLinks.tsx
@@ -10,3 +10,50 @@ interface Link {
   created_at: string;
   updated_at: string;
 }
+
+
+const MyLinks: React.FC = () => {
+    const [links, setLinks] = useState<Link[]>([]);
+    const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+    const [selectedLink, setSelectedLink] = useState<Link | null>(null);
+    const [tagsForLink, setTagsForLink] = useState<{ id: string; name: string }[]>([]);
+    const userId = JSON.parse(localStorage.getItem('userData') || '{}').id;
+
+    useEffect(() => {
+        if (userId) {
+          fetchUserLinks(userId);
+        }
+      }, [userId]);
+    
+      const fetchUserLinks = async (userId: string) => {
+        try {
+          const links = await getUserLinks(userId);
+          setLinks(links);
+        } catch (error) {
+          console.error('Error fetching user links:', error);
+        }
+      };
+    
+      const openTagsPopup = async (link: Link) => {
+        setSelectedLink(link);
+        try {
+          const tags = await getTagsForLink(link.id.toString());
+          setTagsForLink(tags);
+        } catch (error) {
+          console.error('Error fetching tags for link:', error);
+        }
+        setIsPopupOpen(true);
+      };
+    
+      const closeTagsPopup = () => {
+        setIsPopupOpen(false);
+        setSelectedLink(null);
+      };
+    
+      const handleTagUpdate = async (updatedTags: { id: string; name: string }[]) => {
+        setTagsForLink(updatedTags);
+        if (selectedLink) {
+          await fetchUserLinks(userId);
+        }
+      };
+    
