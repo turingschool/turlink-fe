51c7cca0e69e5c7e39cffd8aceb53f6ef5f309f5
Zach Wolek <zach.wolek@gmail.com>
2024-09-11 16:29:17 -0500

    refactor: fix Copy Message so it shows below button

--- src/Components/CopyLink/CopyLink.tsx
+++ src/Components/CopyLink/CopyLink.tsx
@@ -19,28 +19,26 @@ const CopyLink: React.FC<CopyLinkProps>= ({shortenedLink, originalLink}) => {
 
     return (
 <div className='copy-link-body'> 
-    <div className='shortened-link-wrapper'>
-        <div className='shortened-link-container'>
-            <input 
-                type="text" 
-                defaultValue={shortenedLink}
-                className="shortened-link-input"
-                placeholder="Shorten Link Above"
-                readOnly
-            />
-            <button className='copy-button'
-                onClick={handleCopy}
-            >Copy</button>
-            <p className='copy-message-body'>
-                {copyMessage}
-            </p> 
-        </div>
-        <div className='original-link-container'>
-            <h2 className='original-link-label'>Original URL:</h2>
-            <p className='original-link-body'>
-                {originalLink}
-            </p>
-        </div>
+    <div className='shortened-link-container'>
+        <input 
+            type="text" 
+            defaultValue={shortenedLink}
+            className="shortened-link-input"
+            placeholder="Shorten Link Above"
+            readOnly
+        />
+        <button className='copy-button' onClick={handleCopy}>
+            Copy
+        </button>
+    </div>
+    <p className='copy-message-body'>
+        {copyMessage}
+    </p> 
+    <div className='original-link-container'>
+        <h2 className='original-link-label'>Original URL:</h2>
+        <p className='original-link-body'>
+            {originalLink}
+        </p>
     </div>
 </div>
     )
