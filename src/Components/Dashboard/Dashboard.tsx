5e2d69c300ae3cbcc5ae9cf688366a333ad99d8c
David Swatzell <thatsstrikethree@gmail.com>
2024-09-16 16:29:41 -0500

    add back useeffects

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -18,3 +18,27 @@ const Dashboard: React.FC = () => {
     const [tags, setTags] = useState<Tag[]>([]);
     const [selectedTags, setSelectedTags] = useState<string[]>([]);
     const [error, setError] = useState<string>("");
+
+    useEffect(() => {
+        fetchTopLinks(selectedTags.length > 0 ? selectedTags : undefined)
+            .then((fetchedLinks) => {
+                if (fetchedLinks.length === 0) {
+                    setError("No links were found.");
+                } else {
+                    setLinks(fetchedLinks);
+                    setError("");
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                setError("We encountered an unexpected error and were unable to load the links. Please try again later.");
+            });
+    }, [selectedTags]);
+
+    useEffect(() => {
+        fetchTags().then((fetchedTags) => setTags(fetchedTags));
+    }, []);
+
+    
+
+    export default Dashboard;
