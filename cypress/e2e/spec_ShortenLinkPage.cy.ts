e27f0afbb5f3e9785a72daecdbfcd03903870de2
Zach Wolek <zach.wolek@gmail.com>
2024-09-17 12:18:48 -0500

    testing: Completes testing for first entered original link and returned shortened link

--- cypress/e2e/spec_ShortenLinkPage.cy.ts
+++ cypress/e2e/spec_ShortenLinkPage.cy.ts
@@ -1,7 +1,6 @@
 describe('Login Page Tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/login')
-
         cy.intercept("POST", "https://turlink-be-53ba7254a7c1.herokuapp.com/api/v1/sessions", {
             statusCode: 201,
             body: {
@@ -11,14 +10,10 @@ describe('Login Page Tests', () => {
                 }
             }
         })
-        cy.intercept('GET', 'https://turlink-be-53ba7254a7c1.herokuapp.com/api/v1/top_links', {
+        cy.intercept("POST", "https://turlink-be-53ba7254a7c1.herokuapp.com/api/v1/users/undefined/links?link=www.example.com%2FCypressExample", {
             statusCode: 200,
-            fixture: 'topfivelinks'
-        })
-        cy.intercept('GET', 'https://turlink-be-53ba7254a7c1.herokuapp.com/api/v1/tags', {
-            statusCode: 200,
-            fixture: 'tags'
-        })
+            fixture: 'shortenlink.json'  
+          })
         cy.get('.email-input').type('kim@example.com')
         cy.get('.password-input').type('kim123')
         cy.get('.login-button').click()
@@ -35,5 +30,11 @@ describe('Login Page Tests', () => {
         cy.get('.copy-button').should('contain', "Copy")
         cy.get('.original-link-label').should('contain', 'Original URL:')
     })
-    
+    it('should submit an original link and return a shortened link', () => {
+        cy.get('.navbar')
+        cy.get('[href="/shortenlink"]').click()
+        cy.get('.shorten-link-input').type('www.example.com/CypressExample')
+        cy.get('.shorten-link-button').click()
+        cy.get('.shortened-link-input').should('have.value', 'tur.link/4a7c204baeacaf2c')
+    })
 })
