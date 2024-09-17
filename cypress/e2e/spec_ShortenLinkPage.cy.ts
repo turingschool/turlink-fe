17701625a120ccf20c1b613f21f6988efeddd7cf
Zach Wolek <zach.wolek@gmail.com>
2024-09-17 09:46:09 -0500

    testing: log in steps and intercepts are now a 'Before Each' step for testing ShortenLinkPage

--- cypress/e2e/spec_ShortenLinkPage.cy.ts
+++ cypress/e2e/spec_ShortenLinkPage.cy.ts
@@ -1,16 +1,7 @@
 describe('Login Page Tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/login')
-    })
-    it('should display a login form with an email input, a password input, and a submit button', () => {
-        cy.get('.login-header').should('contain', 'Login')
-        cy.get('.email-label').should('contain', 'Email')
-        cy.get('.email-input').should('be.visible')
-        cy.get('.password-label').should('contain', 'Password')
-        cy.get('.password-input').should('be.visible')
-        cy.get('.login-button').should('contain', 'Login')
-    })
-    it('should successfully authenticate a user and route them to their dashboard, if the correct email and password are entered', () => {
+
         cy.intercept("POST", "https://turlink-be-53ba7254a7c1.herokuapp.com/api/v1/sessions", {
             statusCode: 201,
             body: {
@@ -33,5 +24,8 @@ describe('Login Page Tests', () => {
         cy.get('.login-button').click()
         cy.url().should('include', 'dashboard')
         cy.get('.dashboard-header').should('contain', 'Dashboard')
+    })
+    it('should access the shorten link page upon ', () => {
+        cy.get('.navbar')
     })
 })
